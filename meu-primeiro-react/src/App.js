5c53d8352248f94da7316fe7e205079f6d67d1a7
LUZENLUZ <luidyliraa@gmail.com>
2026-09-23 21:44:40 -0300

    Update App.js

--- meu-primeiro-react/src/App.js
+++ meu-primeiro-react/src/App.js
@@ -2,14 +2,28 @@
 import './App.css';
 
 function App() {
+  const names2 = 'joseph';
+
+  function som (a, b) {
+    return a + b
+  }
+  som = 1 + 5
+  
+  
   return (
     <div className="App">
       <header className="App-header">
       
-     <h1> My Home</h1>
+     <div>
+<h2> ola</h2>
+      
+<p>a conta da {som}</p>
+      <p>ola eu sou o {names2}</p>
+     </div>
+    
       </header>
     </div>
-  );
+  );  
 }
 
 export default App;
